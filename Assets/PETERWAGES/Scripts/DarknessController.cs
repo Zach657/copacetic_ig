ac635e0ab82c76e89bb25ab1eb7547b3a89c566f
Peter Wages <pwages@elon.edu>
2016-11-09 23:41:40 -0500

    Fixed player camera look, added Darkness model, fixed player step sound timing, added the ability for Darkness to track the player - currently does not stop when player looks at him

---
+++ Assets/PETERWAGES/Scripts/DarknessController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+/** 
+ * Copyright (C) 2016 - Peter Wages
+ **/
+
+public class DarknessController : MonoBehaviour {
+    public bool playerIsVisible = false;
+	
+	// Freezes Darkness if player can see enemy
+	void Update () {
+	if (playerIsVisible) ActivateTracking(false);
+    else ActivateTracking(true);
+	}
+
+    // Toggles the script responsible for tracking the player
+    private void ActivateTracking(bool activate)
+    {
+        this.GetComponent<AICharacterControl>().enabled = activate;
+    }
+}
