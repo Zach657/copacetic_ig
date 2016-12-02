6b5250877b7ec3434d054c8f5adce0b3276304fb
Your Name <jgreenwell@elon.edu>
2016-12-02 10:51:50 -0500

    removed some things we didnt need

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -5,12 +5,18 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 {
     [RequireComponent(typeof (NavMeshAgent))]
     [RequireComponent(typeof (ThirdPersonCharacter))]
+
+
+
     public class AICharacterControl : MonoBehaviour
     {
-        public NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
+        public NavMeshAgent agent { get; private set; } // the navmesh agent required for the path finding
         public ThirdPersonCharacter character { get; private set; } // the character we are controlling
-        public Transform target;                                    // target to aim for
+        public Transform target; // target to aim for
 
+		private const float TARGETUPDATETIMER = 1000;
+
+		private float timeSinceUpdate = 0;
 
         private void Start()
         {
@@ -20,18 +26,26 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 	        agent.updateRotation = false;
 	        agent.updatePosition = true;
+
+			agent.SetDestination (target.position);
         }
 
 
         private void Update()
         {
-            if (target != null)
-                agent.SetDestination(target.position);
-
-            if (agent.remainingDistance > agent.stoppingDistance)
-                character.Move(agent.desiredVelocity, false, false);
-            else
-                character.Move(Vector3.zero, false, false);
+			//Slight edits made to fix the AI while player is moving a lot
+			if (timeSinceUpdate > TARGETUPDATETIMER) {
+				Debug.Log ("hit");
+				timeSinceUpdate = 0;
+				agent.SetDestination (target.position);
+			}
+			if (agent.remainingDistance > agent.stoppingDistance) {
+				character.Move (agent.desiredVelocity, false, false);
+			}
+			else {
+				character.Move (Vector3.zero, false, false);
+			}
+			timeSinceUpdate = timeSinceUpdate + Time.deltaTime;
         }
 
 
