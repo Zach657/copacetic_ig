7636c47b932d8daa78f3200a8ffba9329f430f8e
Your Name <jgreenwell@elon.edu>
2016-12-07 02:15:31 -0500

    added that yung playback sound in my unity scene, waiting for access to the outside scene to implement it into das prefab

---
+++ Assets/JAMESGREENWELL/Scripts/PlayerMimic.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+//using UnityEngine.Microphone;
+using System.Collections;
+
+public class PlayerMimic : MonoBehaviour {
+
+	//Most of this code is found on https://support.unity3d.com/hc/en-us/articles/206485253-How-do-I-get-Unity-to-playback-a-Microphone-input-in-real-time-
+	void Start () {
+		AudioSource audioSource = this.GetComponent<AudioSource> ();
+		audioSource.clip = Microphone.Start ("Built-in Microphone", true, 10, 44100);
+		audioSource.loop = true;
+		while(!(Microphone.GetPosition(null)>500)){}
+		audioSource.Play ();
+	}
+}
