97479f96aa8c94723de3fda73516ee9533f38bd2
Your Name <jgreenwell@elon.edu>
2016-12-07 23:44:31 -0500

    added player voice playback v-creepy

--- Assets/JAMESGREENWELL/Scripts/PlayerMimic.cs
+++ Assets/JAMESGREENWELL/Scripts/PlayerMimic.cs
@@ -3,14 +3,17 @@
 using System.Collections;
 
 public class PlayerMimic : MonoBehaviour {
-
-	private const int MICDELAY = 1;
+	private AudioSource audioSource;
+	private const int MICDELAY = 500;
 	//Most of this code is found on https://support.unity3d.com/hc/en-us/articles/206485253-How-do-I-get-Unity-to-playback-a-Microphone-input-in-real-time-
 	void Start () {
-		AudioSource audioSource = this.GetComponent<AudioSource> ();
-		audioSource.clip = Microphone.Start ("Built-in Microphone", true, 10, 44100);
+		audioSource = this.GetComponent<AudioSource> ();
+		audioSource.clip = Microphone.Start (null, true, 10, 44100);
 		audioSource.loop = true;
 		while(!(Microphone.GetPosition(null)>MICDELAY)){}
 		audioSource.Play ();
+	}
+	void Update(){
+		audioSource.pitch = 1;
 	}
 }
